5b672976a362f3373efc5ff42d3cfa2e2bed4b74
h3z <hzzz0823@gmail.com>
2022-05-26 16:02:58 +0800

    log early stopping's loss&val_loss
    Signed-off-by: h3z <hzzz0823@gmail.com>

--- callback/wandb_callback.py
+++ callback/wandb_callback.py
@@ -1,6 +1,6 @@
 import numpy as np
-import wandb
 
+import wandb
 from callback.callback import Callback
 from model.base_model import BaseModelApp
 
@@ -13,6 +13,7 @@ class WandbCallback(Callback):
         self.val_batch_losses = []
 
         self.max_pred = []
+        self.min_loss = [np.inf, np.inf]
 
     def on_val_end(self, preds: np.ndarray, gts: np.ndarray, loss):
         # gts = gts.detach().cpu()
@@ -30,6 +31,9 @@ class WandbCallback(Callback):
         self.train_batch_losses.append(loss)
 
     def on_epoch_end(self, loss, val_loss, model_app: BaseModelApp) -> bool:
+        if val_loss < self.min_loss[1]:
+            self.min_loss = [loss, val_loss]
+
         self.val_epoch_losses.append(val_loss)
         self.train_epoch_losses.append(loss)
         wandb.log(
@@ -44,4 +48,10 @@ class WandbCallback(Callback):
 
     def on_train_finish(self, model):
         for i in range(len(self.max_pred)):
-            wandb.log({"max_pred": self.max_pred[i], "max_gt": self.max_gt})
+            wandb.log(
+                {
+                    "max_pred": self.max_pred[i],
+                    "max_gt": self.max_gt,
+                    "min_loss": self.min_loss,
+                }
+            )
