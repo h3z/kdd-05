0110a18e67dfcf0c83ff8b11a9b5d3a0782d983c
h3z <hzzz0823@gmail.com>
2022-06-10 10:22:34 +0800

    .

--- callback/cache_checkpoints.py
+++ callback/cache_checkpoints.py
@@ -11,9 +11,13 @@ class CacheCheckpoints(Callback):
         self.n_iter = 0
 
     def on_epoch_end(self, loss, val_loss, model: BaseModelApp) -> bool:
+        turbine = (
+            f"turbine_{global_config.turbine if global_config.turbine else 'all' }_"
+        )
+        cuda = f"cuda_{global_config.cuda_rank}_" if global_config.distributed else ""
         torch.save(
             model.checkpoint(),
-            f"{global_config.checkpoints_dir}/{global_config.turbine}_{global_config.cuda_rank}_{self.n_iter}.pt",
+            f"{global_config.checkpoints_dir}/{turbine}{cuda}{self.n_iter}.pt",
         )
         self.n_iter += 1
         return True
